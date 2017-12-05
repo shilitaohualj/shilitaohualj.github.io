e21f077b5024a07db4a8c91620ee464d11725fb0
十里桃花 <605877436@qq.com>
2017-12-05 10:19:26 +0800

    新增项目

---
+++ 12.5/js/js.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      autoplay: {
+      delay: 1000,
+     },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+     navigation: {
+      nextEl: '.swiper-button-next'
+    },
+    
+    });
