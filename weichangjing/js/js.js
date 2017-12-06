d9a85208ec0076a8f91e4c0d241141df59401a31
十里桃花 <605877436@qq.com>
2017-12-06 11:06:25 +0800

    更改

--- weichangjing/js/js.js
+++ weichangjing/js/js.js
@@ -1,8 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
       direction: 'vertical',
-      autoplay: {
-      delay: 1000,
-     },
+     // autoplay: {
+     // delay: 1000,
+     //},
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
