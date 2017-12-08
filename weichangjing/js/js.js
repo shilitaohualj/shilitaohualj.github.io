2464da0938eb84daa70f2df52f07dbdcfebbeeae
十里桃花 <605877436@qq>
2017-12-08 11:30:04 +0800

    第一屏动画效果

--- weichangjing/js/js.js
+++ weichangjing/js/js.js
@@ -10,5 +10,15 @@ var swiper = new Swiper('.swiper-container', {
      navigation: {
       nextEl: '.swiper-button-next'
     },
-    
+
+    //Swiper4.x 动画插件
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
     });
